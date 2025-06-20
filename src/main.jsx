adf9b30d54f66844f4daebec7b708d374f6f0072
Dimitri Russell <dimitrianthonyrussell@yahoo.com>
2025-06-20 15:20:58 -0500

    aws bucket fixes

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ import "../public/css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/muay-thai-website">
+    <BrowserRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/muay-thai-website",
+  base: "/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
